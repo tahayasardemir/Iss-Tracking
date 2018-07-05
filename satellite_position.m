fd767788be5de476a68d2f0b7560e985f6ea0a25
Taha Demir <tahayasardemir@gmail.com>
2018-07-05 16:12:17 +0100

    Implemented a code such that reads up to date TLE data for calculation Iss state vectors

--- satellite_position.m
+++ satellite_position.m
@@ -1,16 +1,19 @@
 function satellite_position()
 global mu a n
+format long e
 mu= 398600; % standart gravitional parameter 
+data=['http://www.celestrak.com/NORAD/elements/stations.txt'];
+tle=webread(data); % read up to date TLE data
 % convert all the data from degree to rad
-Me = 204.2868*(pi/180); % Mean anomaly from TLE data (rad)
-n = 15.54 ; % from TLE data (rev/day)
+Me = str2num(tle(1,141:148))*(pi/180); % Mean anomaly from TLE data (rad)
+n = str2num(tle(1,150:160)); % mean motion from TLE data (rev/day)
 a = semimajor(n); % calculate semimajor axis in km
-e = 0.0003435; % from TLE data
+e = str2num(tle(1,127:130))*10^(-7); % eccentricity from TLE data
 h = angularmomentum(a,e); % calculating angular momentum 
-RA = 295.8524*(pi/180); % from TLE data (rad)
-incl = 51.6414*(pi/180); % from TLE data(rad)
-w = 262.6267*(pi/180); % from TLE data (rad)
-TA = trueanomaly(e,Me); %calculated using TLE data (rad)
+RA = str2num(tle(1,115:122))*(pi/180); % Right ascension of the ascending node from TLE data (rad)
+incl = str2num(tle(1,107:113)); % inclination from TLE data(rad)
+w = str2num(tle(1,132:139))*(pi/180); % argument of perigee from TLE data (rad)
+TA = trueanomaly(e,Me); % true anomaly calculated using TLE data (rad)
 [r,v] = rvfromceo (h,e,RA,incl,w,TA);
 t=5; % for the sake of example
 [R,V] = rv_from_r0v0 (r,v,t);
