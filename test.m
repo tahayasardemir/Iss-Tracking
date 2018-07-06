5728da811dadd65d49b49ecb2a5d9f7fe9e467ff
Ferhat9503 <40623327+Ferhat9503@users.noreply.github.com>
2018-07-06 11:39:48 +0300

    Servo test

---
+++ test.m
@@ -0,0 +1,23 @@
+servoAttach(a,4);
+servoAttach(a,5);
+servoAttach(a,6);
+servoAttach(a,7);
+
+% deneme açýlarý
+K=96;
+L=70;
+c=K/2;
+d=K/2;
+x=L/2;
+y=L/2 ;
+
+ servoWrite(a,4,c);
+       
+ servoWrite(a,5,d);
+       
+ servoWrite(a,6,x);
+ servoWrite(a,7,y);
+
+
+
+
