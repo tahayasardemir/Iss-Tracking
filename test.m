86d92c4be1306ab342299e04ae200569681fe4cf
Ferhat9503 <40623327+Ferhat9503@users.noreply.github.com>
2018-07-09 10:20:33 +0300

    srvo sürme kodu

--- test.m
+++ test.m
@@ -4,19 +4,36 @@ servoAttach(a,6);
 servoAttach(a,7);
 
 % deneme açýlarý
-K=96;
-L=70;
+K=180;
+L=0;
 c=K/2;
 d=K/2;
 x=L/2;
-y=L/2 ;
+y=L/2;
 
- servoWrite(a,4,c);
-       
- servoWrite(a,5,d);
-       
- servoWrite(a,6,x);
+if K<0 
+    c=180;
+    d=180+K;
+
+   
+    
+    if L<0
+        x=180;
+        y=180+L;
+    end
+end
+      
+  
+    
+servoWrite(a,4,c);
+ servoWrite(a,5,d); 
+ servoWrite(a,6,x);    
  servoWrite(a,7,y);
+
+ 
+ 
+
+     
 
 
 
