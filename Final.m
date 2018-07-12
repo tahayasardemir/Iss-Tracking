e4626edbfbfb26f02abd5e14e923b81197701dcf
Taha Demir <tahayasardemir@gmail.com>
2018-07-12 10:05:43 +0100

    Added a counter and some comments

--- Final.m
+++ Final.m
@@ -1,23 +1,30 @@
 clc
 clear all
-a=arduino('COM4');
+a=arduino('COM4'); % Initialize arduino
+for count=1:3600 % To run the code automatically
+    
+% Attach the servos to Arduino pins
 servoAttach(a,4);
 servoAttach(a,5);
 servoAttach(a,6);
 servoAttach(a,7);
-time=clock;
-[x,y]=look_angles(time); 
-x;
-y;
-% deneme aç?lar?
-Initial_x=90;
-Initial_y=90;
-Az=Initial_x+(x/2);
-El=Initial_y-((y/2));
+
+time=clock; % Get local time in order to call the functions
+
+[x,y]=look_angles(time); % Calls the function and get Azimuth and Elevation angles
+
+x; % Azimuth 
+y; % Elevation
+
+Initial_x=90; % Azimuth Referance point of the system 
+Initial_y=90; % Elevation referance point of the system
+Az=Initial_x+(x/2); % Azimuth output
+El=Initial_y-((y/2)); % Elevation output
   
 servoWrite(a,6,Az);
 servoWrite(a,7,Az); 
 servoWrite(a,4,El);    
 servoWrite(a,5,El);
+end
 
  
